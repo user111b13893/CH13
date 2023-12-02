5ab04910d9a9c33c5805b1b7ca5875d524acbdfb
Yu <145783104+user111b13893@users.noreply.github.com>
2023-12-02 22:57:04 +0800

    Update Ex13_7.java

--- Ex13_7.java
+++ Ex13_7.java
@@ -1,1 +1,19 @@
+public class Ex13_7 {
+    public static void main(String args[]) {
+        int[] arr = {4, 12, 87, 21, 6, 18};
+        int[] den = {2, 0, 7, 0, 61, 0};
+        double sum = 0.0;
 
+        for (int i = 0; i <= 6; i++) {
+            try {
+                sum += (double) arr[i] / den[i];
+            } catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
+                System.out.println("捕捉到例外了: " + e.getMessage());
+            } catch (Exception e) {
+                System.out.println("其他例外: " + e.getMessage());
+            }
+        }
+
+        System.out.println("Sum=" + sum);
+    }
+}
